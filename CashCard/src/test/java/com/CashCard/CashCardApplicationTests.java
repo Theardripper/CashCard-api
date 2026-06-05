4047026117b54829829935ba708288ab6a289b2c
Theardripper <eduardomoura512@outlook.com>
2026-06-04 21:43:12 -0300

    feat: initial configuration

---
+++ CashCard/src/test/java/com/CashCard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CashCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
